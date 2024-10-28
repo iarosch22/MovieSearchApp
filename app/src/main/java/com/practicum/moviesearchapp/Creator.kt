0107dc8b456cd0abd282bd8ab7a5e5bf33e0b6da
iarosch <vit.iarosch@yandex.by>
2024-10-28 14:14:29 +0300

    refactoring: add controllers for MVC architecture

--- app/src/main/java/com/practicum/moviesearchapp/Creator.kt
+++ app/src/main/java/com/practicum/moviesearchapp/Creator.kt
@@ -1,10 +1,14 @@
 package com.practicum.moviesearchapp
 
+import android.app.Activity
 import com.practicum.moviesearchapp.data.network.MoviesRepositoryImpl
 import com.practicum.moviesearchapp.data.network.RetrofitNetworkClient
 import com.practicum.moviesearchapp.domain.api.MoviesInteractor
 import com.practicum.moviesearchapp.domain.api.MoviesRepository
 import com.practicum.moviesearchapp.domain.impl.MoviesInteractorImpl
+import com.practicum.moviesearchapp.presentation.MoviesSearchController
+import com.practicum.moviesearchapp.ui.movies.MoviesAdapter
+import com.practicum.moviesearchapp.ui.poster.PosterController
 
 object Creator {
     private fun getMoviesRepository(): MoviesRepository {
@@ -13,5 +17,13 @@ object Creator {
 
     fun provideMoviesInteractor(): MoviesInteractor {
         return MoviesInteractorImpl(getMoviesRepository())
+    }
+
+    fun provideMoviesSearchController(activity: Activity, adapter: MoviesAdapter): MoviesSearchController {
+        return MoviesSearchController(activity, adapter)
+    }
+
+    fun providePosterController(activity: Activity): PosterController {
+        return PosterController(activity)
     }
 }
--- app/src/main/java/com/practicum/moviesearchapp/ui/movies/MoviesActivity.kt
+++ app/src/main/java/com/practicum/moviesearchapp/ui/movies/MoviesActivity.kt
@@ -14,10 +14,12 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.practicum.moviesearchapp.Creator
 import com.practicum.moviesearchapp.ui.poster.PosterActivity
 import com.practicum.moviesearchapp.R
 import com.practicum.moviesearchapp.data.dto.MoviesSearchResponse
 import com.practicum.moviesearchapp.data.network.IMDbApiService
+import com.practicum.moviesearchapp.domain.api.MoviesInteractor
 import com.practicum.moviesearchapp.domain.models.Movie
 import retrofit2.Call
 import retrofit2.Callback
@@ -27,26 +29,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MoviesActivity : Activity() {
 
-    private val imdbBaseUrl = "https://tv-api.com"
-
     companion object {
         private const val CLICK_DEBOUNCE_DELAY = 1000L
-        private const val SEARCH_DEBOUNCE_DELAY = 2000L
     }
-
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(imdbBaseUrl)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    private val imdbService = retrofit.create(IMDbApiService::class.java)
-
-    private lateinit var queryInput: EditText
-    private lateinit var placeholderMessage: TextView
-    private lateinit var moviesList: RecyclerView
-    private lateinit var progressBar: ProgressBar
-
-    private val movies = ArrayList<Movie>()
 
     private val adapter = MoviesAdapter {
         if (clickDebounce()) {
@@ -60,95 +45,18 @@ class MoviesActivity : Activity() {
 
     private val handler = Handler(Looper.getMainLooper())
 
-    private val searchRunnable = Runnable { searchRequest() }
-
+    private val moviesSearchController = Creator.provideMoviesSearchController(this, adapter)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies)
 
-        placeholderMessage = findViewById(R.id.placeholderMessage)
-        queryInput = findViewById(R.id.queryInput)
-        moviesList = findViewById(R.id.locations)
-        progressBar = findViewById(R.id.progressBar)
-
-        adapter.movies = movies
-
-        moviesList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        moviesList.adapter = adapter
-
-        queryInput.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                searchDebounce()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-            }
-
-        })
+        moviesSearchController.onCreate()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        handler.removeCallbacks(searchRunnable)
-    }
-
-    private fun searchRequest() {
-        if (queryInput.text.isNotEmpty()) {
-
-            placeholderMessage.visibility = View.GONE
-            moviesList.visibility = View.GONE
-            progressBar.visibility = View.VISIBLE
-
-            imdbService.searchMovies(queryInput.text.toString()).enqueue(object : Callback<MoviesSearchResponse> {
-                override fun onResponse(call: Call<MoviesSearchResponse>,
-                                        response: Response<MoviesSearchResponse>) {
-                    progressBar.visibility = View.GONE
-                    if (response.code() == 200) {
-                        movies.clear()
-                        if (response.body()?.results?.isNotEmpty() == true) {
-                            moviesList.visibility = View.VISIBLE
-                            movies.addAll(response.body()?.results!!)
-                            adapter.notifyDataSetChanged()
-                        }
-                        if (movies.isEmpty()) {
-                            showMessage(getString(R.string.nothing_found), "")
-                        } else {
-                            hideMessage()
-                        }
-                    } else {
-                        showMessage(getString(R.string.something_went_wrong), response.code().toString())
-                    }
-                }
-
-                override fun onFailure(call: Call<MoviesSearchResponse>, t: Throwable) {
-                    progressBar.visibility = View.GONE
-                    showMessage(getString(R.string.something_went_wrong), t.message.toString())
-                }
-            })
-        }
-    }
-
-    private fun showMessage(text: String, additionalMessage: String) {
-        if (text.isNotEmpty()) {
-            placeholderMessage.visibility = View.VISIBLE
-            movies.clear()
-            adapter.notifyDataSetChanged()
-            placeholderMessage.text = text
-            if (additionalMessage.isNotEmpty()) {
-                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
-                    .show()
-            }
-        } else {
-            placeholderMessage.visibility = View.GONE
-        }
-    }
-
-    private fun hideMessage() {
-        placeholderMessage.visibility = View.GONE
+        moviesSearchController.onDestroy()
     }
 
     private fun clickDebounce() : Boolean {
@@ -160,8 +68,4 @@ class MoviesActivity : Activity() {
         return current
     }
 
-    private fun searchDebounce() {
-        handler.removeCallbacks(searchRunnable)
-        handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
-    }
 }
