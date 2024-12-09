03aab996d35df350907b748b52f6c06084065b43
iarosch <vit.iarosch@yandex.by>
2024-12-09 14:12:25 +0300

    feat: add rootActivity

---
+++ app/src/main/java/com/practicum/moviesearchapp/ui/root/RootActivity.kt
@@ -0,0 +1,15 @@
+package com.practicum.moviesearchapp.ui.root
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.practicum.moviesearchapp.R
+
+class RootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+    }
+}
